0d97134d4597cec0a53eee43e0a1826b37fbcd84
Mr.Lutin <mcotemw@icloud.com>
2023-12-02 16:12:14 -0500

    First commit
    Initial data for the demo

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+-- FX Information --
+fx_version   'cerulean'
+lua54        'yes'
+use_fxv2_oal 'yes'
+games        { 'rdr3', 'gta5' }
+
+-- Resource Information --
+version      '1.0.0'
+name         'mrlutin_demo'
+author       'MrLutin'
+repository   "https://github.com/MrLutin/mrlutin_demo"
+description  'A demo, made to show some functionality of mrlutin_lib'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+-- Manifest --
+dependencies {
+    '/onesync',
+    'mrlutin_lib',
+}
+
+shared_scripts {
+    '@mrlutin_lib/imports.lua',
+    'config.lua',
+}
+
+client_script 'client/main.lua'
+
+server_script 'server/main.lua'
