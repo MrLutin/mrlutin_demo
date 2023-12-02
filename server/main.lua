d39d44cbbc1b7b0061ea36a65c580306891855ce
Mr.Lutin <mcotemw@icloud.com>
2023-12-02 16:40:20 -0500

    add basic file

---
+++ server/main.lua
@@ -0,0 +1,6 @@
+-- Load all local data
+Mrlutin.locale()
+
+-- check if dependencies for some other resources
+Mrlutin.checkDependency('mrlutin_lib', '1.0.0')
+Mrlutin.checkDependency('ox_lib', '3.12.0')
